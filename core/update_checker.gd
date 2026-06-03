6556688826966e50bbebaf76e1469f519ba1d4d2
NightMin2002 <3091802204@qq.com>
2026-06-03 12:56:22 +0800

    release: v2.2

--- core/update_checker.gd
+++ core/update_checker.gd
@@ -3,8 +3,8 @@
 extends Node
 
 ## 当前版本号 (发版时同步修改此处 + installer.iss)
-const CURRENT_VERSION := "2.1"
-const COMMIT_COUNT := 365  ## 发版时由 release.ps1 自动更新
+const CURRENT_VERSION := "2.2"
+const COMMIT_COUNT := 369  ## 发版时由 release.ps1 自动更新
 
 ## GitHub 仓库信息
 const REPO_OWNER := "NightMin2002"
